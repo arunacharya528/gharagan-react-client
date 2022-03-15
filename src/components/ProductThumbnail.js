7d47bc40cc953732377cef7a164c501ebfd7d78f
Arun <arunacharya255@gmail.com>
2022-03-15 21:50:46 +0545

    Added most viewed and latest product on homepage

--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const ProductThumbnail = (props) => {
@@ -5,10 +6,16 @@ export const ProductThumbnail = (props) => {
     const discountPercent = props.product.discount.discount_percent;
     const actualPrice = props.product.price - Math.ceil(props.product.discount.discount_percent * 0.01 * props.product.price)
     const initialPrice = props.product.price;
+
+    const image1 = props.product.images.length > 0 ? props.product.images[0].image : '';
+    const image2 = props.product.images.length >= 2 ? props.product.images[1].image : '';
+
     return (
         <Link to={'/product/' + props.product.id} class="product-thumbnail  col-12 col-sm-6 col-lg-3">
             <div id="image-container">
-                <img src="https://via.placeholder.com/400x200" />
+                <img src={image1} alt={"First Image of " + props.product.name} />
+                <img src={image2} alt={"Second Image of " + props.product.name} />
+
             </div>
             <div class="d-flex justify-content-between">
                 <div id="category">{props.product.category.name}</div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,21 @@ import { ProductThumbnail } from "../components/ProductThumbnail";
 
 export const Home = () => {
 
-    const [products, setProducts] = useState([]);
+    const [latestProducts, setLatestProducts] = useState([]);
+    const [mostViewedProducts, setMostViewedProducts] = useState([]);
 
     useEffect(() => {
-        getProducts()
+        getProducts("item=8&page=1&latest=true")
             .then((response) => {
-                console.log(response.data);
-                setProducts(response.data);
+                setLatestProducts(response.data.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        getProducts("item=8&page=1&mostViewed=true")
+            .then((response) => {
+                setMostViewedProducts(response.data.data);
             })
             .catch((error) => {
                 console.log(error);
@@ -22,20 +30,34 @@ export const Home = () => {
     return (
         <>
             <Banner />
-
-
             <div className="container">
 
-                <div class="header">
-                    <h5>Gharagan Sells</h5>
-                    <a href="#">More Products <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
-                </div>
+                <section className="mb-5">
+                    <div class="header">
+                        <h5>Latest Products</h5>
+                        <a href="#">More Products <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
+                    </div>
 
-                <div id="catalog-container" className="row">
-                    {
-                        products.map((product, index) => <ProductThumbnail key={index} product={product} />)
-                    }
-                </div>
+                    <div id="catalog-container" className="row">
+                        {
+                            latestProducts.map((product, index) => <ProductThumbnail key={index} product={product} />)
+                        }
+                    </div>
+                </section>
+
+                <section className="mb-5">
+                    <div class="header">
+                        <h5>Most Viewed Products</h5>
+                        <a href="#">More Products <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
+                    </div>
+
+                    <div id="catalog-container" className="row">
+                        {
+                            mostViewedProducts.map((product, index) => <ProductThumbnail key={index} product={product} />)
+                        }
+                    </div>
+                </section>
+
             </div>
         </>
     );
