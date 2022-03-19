4a73d765a2768cfd2280be8598d2416458fd7910
Arun <arunacharya255@gmail.com>
2022-03-19 16:35:58 +0545

    added method to fetch single product

--- src/adapters/product.js
+++ src/adapters/product.js
@@ -10,3 +10,15 @@ export const getProducts = (urlQuery = '', item = "item=8") => {
     };
     return axios(config)
 }
+
+
+export const getproduct = (id) => {
+    let config = {
+        method: 'get',
+        url: `${apiURL}/product/${id}`,
+        headers: {},
+        maxRedirects: 0
+    };
+
+    return axios(config);
+}
