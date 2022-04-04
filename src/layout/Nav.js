fbfa141ed50c6aeeda0ab055387f6a32d4349e83
Arun <arunacharya255@gmail.com>
2022-04-04 21:54:45 +0545

    Updated nav bar

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -2,17 +2,30 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Cookies from "universal-cookie";
 import { getCategories } from "../adapters/category";
+import { getProducts } from "../adapters/product";
 import { getShoppingSession } from "../adapters/shoppingSession";
+import { ProductThumbnail } from "../components/ProductThumbnail";
+import { Loading } from "../helpers/Loading";
 import { AuthLink, AuthUser } from "../pages/Authenticate";
+
 
 export const Nav = () => {
 
     const [categories, setCategories] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState([]);
+
+    const [latestProducts, setLatestProducts] = useState([]);
+    const [latestProductLink, setLatetstProductlink] = useState('');
+
+    const [mostViewedProducts, setMostViewedProducts] = useState([]);
+    const [mostViewedProductLink, setMostViewedProductLink] = useState('');
+
+
     const [session, setSession] = useState(undefined);
     const navigate = useNavigate();
     const cookie = new Cookies()
     const [userData, setUserData] = useState(undefined);
+
     useEffect(() => {
         getCategories()
             .then(response => {
@@ -39,6 +52,30 @@ export const Nav = () => {
             })
             .catch(error => console.log(error))
     })
+
+    const handleCategorySelection = (category) => {
+        // extract ids of child categories in an array
+        const childCategories = category.child_categories.map((category, index) => {
+            return category.id;
+        })
+
+        // implode the array by joining with string and fetch data with contructed link
+        const latestLink = "categories=" + childCategories.join(',') + "&sort=latest";
+        getProducts(latestLink)
+            .then(response => setLatestProducts(response.data.data))
+            .catch(error => console.log(error))
+        setLatetstProductlink("/filter/?" + latestLink);
+
+
+        const mostViewedLink = "categories=" + childCategories.join(',') + "&sort=mostViewed";
+        getProducts(mostViewedLink)
+            .then(response => setMostViewedProducts(response.data.data))
+            .catch(error => console.log(error))
+        setMostViewedProductLink("/filter/?" + mostViewedLink);
+        setSelectedCategory(category);
+
+        setTimeout()
+    }
 
     return (
         <div id="nav">
@@ -100,10 +137,12 @@ export const Nav = () => {
                         <Link to="/filter/">Filter</Link>
 
                         {categories.map((category, index) =>
-                            <a onClick={e => setSelectedCategory(category)} key={index}>{category.name}</a>
+                            <a onClick={e => handleCategorySelection(category)} key={index}>{category.name}</a>
                         )}
                     </div>
                     {selectedCategory.length !== 0 ?
+
+
                         <div className="selectedCategory">
                             <div className="row">
                                 <div className="col-sm-5 col-lg-3 d-flex flex-column">
@@ -117,16 +156,43 @@ export const Nav = () => {
                                     }
                                 </div>
                                 <div className="col p-3">
-                                    <div className="d-flex justify-content-end">
+                                    <div className="d-flex justify-content-between align-items-start mb-5">
+                                        <p>
+                                            {selectedCategory.description}
+                                        </p>
                                         <button className="btn btn-close text-white" onClick={e =>
                                             setSelectedCategory([])
                                         }></button>
                                     </div>
-                                    <div></div>
-                                    {selectedCategory.description}
+                                    <section className="mb-4">
+                                        <div className="d-flex justify-content-between">
+                                            <h5>Latest Products</h5>
+                                            <Link to={latestProductLink}>More <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></Link>
+                                        </div>
+                                        <div id="catalog-container" className="row">
+                                            {latestProducts.length === 0 ?
+                                                <Loading />
+                                                : latestProducts.slice(0, 3).map((product, index) => <ProductThumbnail key={index} product={product} width={4} />)
+                                            }
+                                        </div>
+                                    </section>
+                                    <section className="mb-4">
+                                        <div className="d-flex justify-content-between">
+                                            <h5>Most Viewed Products</h5>
+                                            <Link to={mostViewedProductLink}>More <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></Link>
+                                        </div>
+                                        <div id="catalog-container" className="row">
+                                            {mostViewedProducts.length === 0 ?
+                                                <Loading />
+                                                : mostViewedProducts.slice(0, 3).map((product, index) => <ProductThumbnail key={index} product={product} width={4} />)
+                                            }
+                                        </div>
+                                    </section>
+
                                 </div>
                             </div>
                         </div>
+
                         : ''}
                 </div>
             </nav>
