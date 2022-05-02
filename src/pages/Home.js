913ab1dc50f3f22d72a45d721df83be22c004b08
Arun <arunacharya255@gmail.com>
2022-05-02 17:41:30 +0545

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,8 @@ export const Home = () => {
 
     const [latestProducts, setLatestProducts] = useState([]);
     const [mostViewedProducts, setMostViewedProducts] = useState([]);
+    const [latestPage, setLatestPage] = useState(1);
+    const [mostViewedPage, setMostViewedPage] = useState(1);
 
     useEffect(() => {
         getProducts("sort=latest")
@@ -29,7 +31,6 @@ export const Home = () => {
             });
     }, [])
 
-    console.log(latestProducts, mostViewedProducts)
     return (
         <>
             <Banner />
@@ -37,13 +38,25 @@ export const Home = () => {
                 <section className="mb-5">
                     <div class="header">
                         <h5>Latest Products</h5>
-                        <Link to="/filter?sort=latest">More Products <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></Link>
                     </div>
 
                     <div id="catalog-container" className="row">
                         {latestProducts.length === 0 ?
                             <Loading />
-                            : latestProducts.map((product, index) => <ProductThumbnail key={index} product={product} />)
+                            : latestProducts.slice(0, (latestPage * 8)).map((product, index) => <ProductThumbnail key={index} product={product} />)
+                        }
+                    </div>
+
+                    <div className="d-flex justify-content-center my-3">
+                        {
+                            latestPage < latestProducts.length / 8 ?
+                                <button className="btn mx-2 btn-outline-primary" onClick={() => { setLatestPage(latestPage + 1) }}>View More</button>
+                                : ''
+                        }
+                        {
+                            latestPage > 1 ?
+                                <button className="btn mx-2 btn-outline-primary" onClick={() => { setLatestPage(1) }}>Set default</button>
+                                : ''
                         }
                     </div>
                 </section>
@@ -51,15 +64,34 @@ export const Home = () => {
                 <section className="mb-5">
                     <div class="header">
                         <h5>Most Viewed Products</h5>
-                        <Link to={"/filter?sort=mostViewed"}>More Products <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></Link>
                     </div>
 
-                    <div id="catalog-container" className="row">
+                    {/* <div id="catalog-container" className="row">
                         {
                             mostViewedProducts.length === 0 ?
                                 <Loading />
                                 :
                                 mostViewedProducts.map((product, index) => <ProductThumbnail key={index} product={product} />)
+                        }
+                    </div> */}
+
+                    <div id="catalog-container" className="row">
+                        {mostViewedProducts.length === 0 ?
+                            <Loading />
+                            : mostViewedProducts.slice(0, (mostViewedPage * 8)).map((product, index) => <ProductThumbnail key={index} product={product} />)
+                        }
+                    </div>
+
+                    <div className="d-flex justify-content-center my-3">
+                        {
+                            mostViewedPage < mostViewedProducts.length / 8 ?
+                                <button className="btn mx-2 btn-outline-primary" onClick={() => { setMostViewedPage(mostViewedPage + 1) }}>View More</button>
+                                : ''
+                        }
+                        {
+                            mostViewedPage > 1 ?
+                                <button className="btn mx-2 btn-outline-primary" onClick={() => { setMostViewedPage(1) }}>Set default</button>
+                                : ''
                         }
                     </div>
                 </section>
