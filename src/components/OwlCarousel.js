7a616018196df730add611b9c17b4fbdac8f5aeb
Arun <arunacharya255@gmail.com>
2022-05-02 21:04:28 +0545

    appearance fix

--- src/components/OwlCarousel.js
+++ src/components/OwlCarousel.js
@@ -5,6 +5,9 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 export const CarouselView = ({ items, displayItems }) => {
+    const leftIcon = `<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>`;
+    const rightIcon = `<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>`;
+
     const options = {
         loop: true,
         nav: true,
@@ -13,6 +16,10 @@ export const CarouselView = ({ items, displayItems }) => {
         lazyload: true,
         autoplayTimeout: 5000,
         autoplayHoverPause: true,
+        navText: [
+            `${leftIcon}`,
+            `${rightIcon}`
+        ],
     };
     return (
         <OwlCarousel className="owl-carousel owl-theme" {...options}>
