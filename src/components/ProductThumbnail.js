c34d35002b0e11a4fc26c00b84573ffbdd2d3a45
Arun <arunacharya255@gmail.com>
2022-05-02 17:26:17 +0545

    Updated product thumbnail

--- src/components/ProductThumbnail.js
+++ src/components/ProductThumbnail.js
@@ -4,19 +4,42 @@ import { RateDisplay, RateDisplayByNumber } from "./Rating";
 
 export const ProductThumbnail = (props) => {
 
-    const image1 = props.product.images.length > 0 ? props.product.images[0].image : '';
-    const image2 = props.product.images.length >= 2 ? props.product.images[1].image : '';
-
+    const [selectedInventory, setSelectedInventory] = useState(null);
+    const images = props.product.images.filter((image, index) => {
+        if (index < 2) {
+            return image;
+        }
+    })
     const averageRating = props.product.averageRating;
 
     const getDiscountedPrice = (mainPrice, discount) => {
         return discount ? mainPrice - Math.ceil(discount.discount_percent * 0.01 * mainPrice) : mainPrice;
     }
+
+    const handleInventoryChange = (inventory) => {
+        setSelectedInventory(inventory)
+    }
+
+    const determineInventoryButton = (inventory) => {
+        if (selectedInventory && selectedInventory.id === inventory.id) {
+            return 'btn-primary'
+        } else {
+            return 'btn-outline-primary'
+        }
+    }
+
+    useEffect(() => {
+        setSelectedInventory(props.product.inventories[0])
+    }, [])
+
     return (
-        <Link to={'/product/' + props.product.id} class={"product-thumbnail  col-12 col-sm-6 col-lg-" + (props.width ? props.width : 3)}>
+        <div to={'/product/' + props.product.id} class={"product-thumbnail  col-12 col-sm-6 col-lg-" + (props.width ? props.width : 3)}>
             <div id="image-container">
-                <img src={image1} alt={"First Image of " + props.product.name} />
-                <img src={image2} alt={"Second Image of " + props.product.name} />
+                {
+                    images.map((image, index) =>
+                        <img src={image.file ? process.env.REACT_APP_FILE_PATH + image.file.name : image.image_url} alt={"Image " + (index + 1) + " of " + props.product.name} key={index} />
+                    )
+                }
             </div>
             <div class="d-flex justify-content-between">
                 <div id="category">{props.product.category.name}</div>
@@ -24,13 +47,34 @@ export const ProductThumbnail = (props) => {
             </div>
 
             <div id="name">{props.product.name}</div>
-            <div id="price" class="d-flex justify-content-between align-items-center">
-                <div class="d-flex">
-                    <div id="actual-price">Rs. {getDiscountedPrice(props.product.price, props.product.discount)}</div>
-                    {props.product.discount ? <div id="original-price"><s>Rs. {props.product.price}</s></div> : ''}
+
+
+            {
+                selectedInventory ?
+                    <div id="price" class="d-flex justify-content-between align-items-center my-2">
+                        <div class="d-flex">
+                            <div id="actual-price">Rs. {getDiscountedPrice(selectedInventory.price, selectedInventory.discount)}</div>
+                            {props.product.discount ? <div id="original-price"><s>Rs. {selectedInventory.price}</s></div> : ''}
+                        </div>
+                        {selectedInventory.discount ? <div id="discount">{selectedInventory.discount.discount_percent}% OFF</div> : ''}
+                    </div>
+                    : ''
+
+            }
+
+            <div className="flex flex-row flex-no-wrap justify-content-between align-items-center mt-3">
+
+                <div className="d-flex justify-content-center flex-wrap">
+                    {props.product.inventories.map((inventory, index) =>
+                        <button type="button" class={"btn btn-sm m-1 " + determineInventoryButton(inventory)} key={index} onClick={e => handleInventoryChange(inventory)}>{inventory.type}</button>
+                    )}
                 </div>
-                {props.product.discount ? <div id="discount">{props.product.discount.discount_percent}% OFF</div> : ''}
+
+                <button className="brand-btn rounded w-100 py-2 mt-2">Add to cart</button>
+                <div className="text-center mt-2">
+                    <Link to={'/product/' + props.product.id}>View product</Link>
+                </div>
             </div>
-        </Link>
+        </div>
     );
 }
