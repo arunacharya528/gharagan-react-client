895c8ce8309318c68eb839aab8b7c44bb56787b7
Arun <arunacharya255@gmail.com>
2022-05-04 16:33:01 +0545

    updated user layout

---
+++ src/icons/index.js
@@ -0,0 +1,14 @@
+import { ReactComponent as BagIcon } from './bag.svg';
+import { ReactComponent as CartIcon } from './cart.svg';
+import { ReactComponent as PersonIcon } from './person.svg';
+import { ReactComponent as MapPinIcon } from './map-pin.svg';
+import { ReactComponent as CreditCardIcon } from './credit-card.svg';
+
+
+export {
+    BagIcon,
+    CartIcon,
+    PersonIcon,
+    MapPinIcon,
+    CreditCardIcon
+}
--- src/layout/UserLayout.js
+++ src/layout/UserLayout.js
@@ -1,18 +1,90 @@
 import { Link, useLocation } from "react-router-dom";
+import { BagIcon, CartIcon, CreditCardIcon, MapPinIcon, PersonIcon } from "../icons";
+import bag from "../icons/bag.svg"
 
 export const UserLayout = ({ component }) => {
     const location = useLocation();
     const section = location.pathname.split("/");
     return (
-        <div className="container-fluid row my-4">
-            <div className="col-lg-2">
-                <div className="d-flex flex-column sticky-top">
-                    <Link to={"/user"} className={"my-1 btn btn-" + (section[1] == "user" ? 'secondary' : 'default')}>User</Link>
-                    <Link to={"/cart"} className={"my-1 btn btn-" + (section[1] == "cart" ? 'secondary' : 'default')}>Cart</Link>
-                    <Link to={"/order"} className={"my-1 btn btn-" + (section[1] == "order" ? 'secondary' : 'default')}>Order</Link>
+        <div className="container my-4">
+            <div className="row">
+                <div className="col-lg-3">
+                    <div id="profile-card">
+
+                        <div id="profile">
+                            <div className="name">Some name</div>
+                            <div className="email">someemail@email.com</div>
+                        </div>
+
+                        <div className="header">Dashboard</div>
+
+                        <div className="grid">
+                            <div className="item">
+                                <div className="detail">
+                                    <div className="icon">
+                                        <BagIcon />
+                                    </div>
+                                    <Link to={"/user"} >Orders</Link>
+                                </div>
+                                <div className="value">
+                                    1
+                                </div>
+                            </div>
+
+                            <div className="item">
+                                <div className="detail">
+                                    <div className="icon">
+                                       <CartIcon/>
+                                    </div>
+                                    <Link to={"/user"} >Cart</Link>
+                                </div>
+                                <div className="value">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <div className="header">Account Settings</div>
+
+                        <div className="grid">
+                            <div className="item">
+                                <div className="detail">
+                                    <div className="icon">
+                                        <PersonIcon/>
+                                    </div>
+                                    <Link to={"/user"} >Profile</Link>
+                                </div>
+                                <div className="value">
+                                    
+                                </div>
+                            </div>
+
+                            <div className="item">
+                                <div className="detail">
+                                    <div className="icon">
+                                        <MapPinIcon />
+                                    </div>
+                                    <Link to={"/user"} >Addresses</Link>
+                                </div>
+                                <div className="value">
+                                    
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="detail">
+                                    <div className="icon">
+                                        <CreditCardIcon />
+                                    </div>
+                                    <Link to={"/user"} >Payment Methods</Link>
+                                </div>
+                                <div className="value">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <div className="col-lg-9">{component}</div>
             </div>
-            <div className="col-lg-10">{component}</div>
         </div>
 
     );
