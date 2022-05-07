bfd0479f241565dd60057bbea0aceec9541367d1
Arun <arunacharya255@gmail.com>
2022-05-07 14:52:30 +0545

    Added new icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,8 +4,9 @@ import { ReactComponent as PersonIcon } from './person.svg';
 import { ReactComponent as MapPinIcon } from './map-pin.svg';
 import { ReactComponent as CreditCardIcon } from './credit-card.svg';
 import { ReactComponent as CloseIcon } from './close.svg';
-
-
+import { ReactComponent as EditIcon } from './pen.svg';
+import { ReactComponent as TrashIcon } from './trash.svg';
+import { ReactComponent as PlusIcon } from './plus.svg';
 
 export {
     BagIcon,
@@ -13,5 +14,8 @@ export {
     PersonIcon,
     MapPinIcon,
     CreditCardIcon,
-    CloseIcon
+    CloseIcon,
+    EditIcon,
+    TrashIcon,
+    PlusIcon
 }
