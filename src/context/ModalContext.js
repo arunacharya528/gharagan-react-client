ae881886752cc0b1afb279144091cd7719edb7f2
Arun <arunacharya255@gmail.com>
2022-05-10 16:57:16 +0545

    updated modal view

--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useEffect, useState } from "react"
 
 export const ModalContext = createContext();
 
@@ -16,8 +16,8 @@ export const ModalProvider = ({ children }) => {
     }
 
     return <ModalContext.Provider value={{ setModalData, openModal, closeModal }}>
-        <div class={"modal modal-bottom sm:modal-middle pointer-events-auto pb-2 " + (isModalOpen ? "visible opacity-100" : "invisible")}>
-            <div class={"modal-box relative" + modalData.size}>
+        <div class={"modal pointer-events-auto pb-2 " + (isModalOpen ? "visible opacity-100" : "invisible")}>
+            <div class={"modal-box relative " + modalData.size}>
                 <button class="btn btn-sm btn-circle absolute right-2 top-2" onClick={e => setIsModalOpen(false)}>✕</button>
                 <h3 class="font-bold text-lg">{modalData.title}</h3>
                 <div className="py-4">{modalData.body}</div>
