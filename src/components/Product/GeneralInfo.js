c93a6d9ff5f0572b23ed0d841cbfa150ff23d6cd
Arun <arunacharya255@gmail.com>
2022-05-30 22:15:37 +0545

    updated type listing

--- src/components/Product/GeneralInfo.js
+++ src/components/Product/GeneralInfo.js
@@ -19,7 +19,7 @@ export const GeneralInfo = ({ product, setSelectedTab }) => {
         <>
             {
                 product.loading ?
-                    <GeneralInfoSkeleton/>
+                    <GeneralInfoSkeleton />
 
                     :
                     <div className="grid md:grid-cols-2 gap-5">
@@ -42,7 +42,7 @@ export const GeneralInfo = ({ product, setSelectedTab }) => {
                                 <a className="font-semibold text-primary cursor-pointer" onClick={e => setSelectedTab(3)}>See all {product.data.ratings.length} reviews</a>
                             </div>
 
-                            {
+                            {/* {
                                 selectedInventory ?
                                     <div class="flex justify-between items-center">
                                         <div class="flex flex-col">
@@ -53,9 +53,9 @@ export const GeneralInfo = ({ product, setSelectedTab }) => {
                                     </div>
                                     : ''
 
-                            }
+                            } */}
 
-                            <div className="flex flex-row flex-wrap">
+                            {/* <div className="flex flex-row flex-wrap">
 
                                 {
                                     product.data.inventories.map((inventory, index) =>
@@ -64,7 +64,31 @@ export const GeneralInfo = ({ product, setSelectedTab }) => {
                                         </button>
                                     )
                                 }
+                            </div> */}
+
+                            <div className="flex flex-col divide-y">
+                                {
+                                    product.data.inventories.map((inventory, index) =>
+
+                                        <div class={"flex justify-between items-center p-3 " + (selectedInventory !== null && inventory.id === selectedInventory.id ? 'bg-base-300' : '')} onClick={e => { setSelectedInventory(inventory) }} key={index}>
+                                            <div className="w-12 px-4">
+                                                {selectedInventory !== null && inventory.id === selectedInventory.id ? "✔" : ''}
+                                            </div>
+                                            <div className="flex-1">{inventory.type}</div>
+                                            <div class="flex flex-col grow">
+                                                <div className="text-accent text-lg font-bold">Rs. {getDiscountedPrice(inventory.price, inventory.discount.discount_percent)}</div>
+                                                {inventory.discount ? <div className="font-light"><s>Rs. {inventory.price}</s></div> : ''}
+                                            </div>
+                                            {inventory.discount ? <div className="badge badge-success badge-outline p-3">{inventory.discount.discount_percent}% OFF</div> : ''}
+                                        </div>
+                                    )
+
+                                }
+
                             </div>
+
+                            <div className="text-sm">Select among various types and add to cart</div>
+
 
                             <div className="flex space-x-2">
                                 <div className="btn btn-primary grow">Add to cart</div>
