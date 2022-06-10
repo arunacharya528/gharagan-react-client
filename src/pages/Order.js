0750fb253c342ebc59c65605a2e623d0f939caf1
Arun <arunacharya255@gmail.com>
2022-06-10 18:49:44 +0545

    Added functionality to cancel order

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -7,16 +7,22 @@ import { getUser } from "../adapters/profile";
 import { UserContext } from "../context/UserContext";
 import { OrderThumbnailSkeleton } from "../components/Skeleton/OrderSkeleton";
 
+import { TrashIcon } from "../icons"
+import toast from "react-hot-toast";
+import { cancelOrder } from "../adapters/orderDetail";
+
 export const Order = () => {
 
     const { user } = useContext(UserContext);
     const [orders, setOrders] = useState({ data: [], loading: true })
+    const [isRereshed, setRefresh] = useState(false);
+
     useEffect(() => {
 
         getUser('', user.id, 'orders')
             .then(response => setOrders({ data: response.data, loading: false }))
             .catch(error => console.log(error))
-    }, []);
+    }, [isRereshed]);
 
     const getSteps = (status) => {
 
@@ -27,12 +33,26 @@ export const Order = () => {
         }
         return (
             <ul class="steps steps-vertical lg:steps-horizontal lg:w-full my-5">
-                <li class={"step " + getResponse(1)}>Order placed</li>
+                <li class={"step " + getResponse(1)}>Order placed<br />(Available for cancellation)</li>
                 <li class={"step " + getResponse(2)}>Product Collected for delivery</li>
                 <li class={"step " + getResponse(3)}>Product being shipped</li>
                 <li class={"step " + getResponse(4)}>Product received</li>
             </ul>
         );
+    }
+
+    const handleCancellation = (id) => {
+        toast.promise(
+            cancelOrder('',id),
+            {
+                loading: "Cancelling order",
+                success: () => {
+                    setRefresh(!isRereshed)
+                    return "Successfully cancelled order"
+                },
+                error: "Error cancelling order"
+            }
+        )
     }
 
     return (
@@ -46,7 +66,7 @@ export const Order = () => {
 
                     :
                     orders.data.map((order, index) =>
-                        <div key={index} className="border rounded-md shadow-sm hover:shadow-md ease-in-out duration-300 divide-y">
+                        <div key={index} className="rounded-md shadow-sm hover:shadow-md ease-in-out duration-300 bg-base-200 divide-y divide-slate-500">
                             <div className="flex flex-col md:flex-row items-center py-5 px-7 space-y-5 md:space-y-0">
 
                                 <div className="flex flex-col md:flex-row space-y-5 md:space-x-10 md:space-y-0 font-semibold grow w-full md:w-auto">
@@ -67,9 +87,14 @@ export const Order = () => {
                                 </div>
 
 
-                                <div className="flex flex-col md:flex-row space-y-5 md:space-x-10 md:space-y-0 w-full md:w-auto">
-                                    <Link to={"/user/orders/" + order.id} className="btn btn-primary btn-outline btn-sm">View Order</Link>
-                                    <button className="btn btn-primary btn-outline btn-sm">View Invoice</button>
+                                <div className="flex flex-col md:flex-row space-y-5 md:space-x-6 md:space-y-0 w-full md:w-auto">
+                                    <Link to={"/user/orders/" + order.id} className="btn btn-ghost btn-outline">View Order</Link>
+
+
+                                    <button className="btn btn-ghost btn-outline">View Invoice</button>
+
+                                    <button className="btn btn-outline btn-error gap-2" disabled={order.status === 1 ? false : true} onClick={e => handleCancellation(order.id)}> <TrashIcon /> Cancel order</button>
+
                                 </div>
                             </div>
 
@@ -77,6 +102,12 @@ export const Order = () => {
                                 {getSteps(order.status)}
                                 <div className="font-semibold text-center">Latest action took place in {moment(order.updated_at).format("MMMM D YYYY")}</div>
                             </div>
+                            {order.status === 1 ? '' :
+                                <div className="p-3 text-center">
+                                    Order cannot be cancelled in this stage. Contact us for more information.
+                                </div>
+                            }
+
                         </div>
                     )
 
