6050e5cf63564cf5f66050146c98e07c7f53a7e1
Arun <arunacharya255@gmail.com>
2022-08-30 12:41:03 +0545

    fixed category preview

--- src/components/Nav/NavProductContainer.js
+++ src/components/Nav/NavProductContainer.js
@@ -4,19 +4,30 @@ import { ShortProductThumbnail } from "../Thumbnail/ShortProductThumbnail";
 
 export const NavProductContainer = ({ title, products, link, forward }) => {
 
-    
+
     return (
-        <section className="mb-4">
-            <div className="flex justify-between py-3">
-                <span className="text-xl font-bold">{title}</span>
-                <span onClick={e => forward(link)} className="btn btn-sm btn-ghost rounded-full">More</span>
-            </div>
-            <div className="flex overflow-auto flex-auto space-x-5">
-                {products.length === 0 ?
-                    <Loading />
-                    : products.slice(0, 3).map((product, index) => <ShortProductThumbnail key={index} product={product} width={4} moveForward={link => forward(link)} />)
-                }
-            </div>
-        </section>
+        <>
+            {
+                products.length === 0 ?
+                    <div className="flex flex-col items-center justify-center py-10">
+                        <span className="text-5xl font-semibold py-5">O.O</span>
+                        No {title} available
+                    </div>
+                    :
+                    <section className="mb-4">
+                        <div className="flex justify-between py-3">
+                            <span className="text-xl font-bold">{title}</span>
+                            <span onClick={e => forward(link)} className="btn btn-sm btn-ghost rounded-full">More</span>
+                        </div>
+                        <div className="grid md:grid-cols-3 gap-5">
+                            {
+                                products.slice(0, 3).map((product, index) => <ShortProductThumbnail key={index} product={product} width={4} moveForward={link => forward(link)} />)
+                            }
+                        </div>
+                    </section>
+
+            }
+        </>
+
     );
 }
--- src/components/Thumbnail/ShortProductThumbnail.js
+++ src/components/Thumbnail/ShortProductThumbnail.js
@@ -9,7 +9,7 @@ export const ShortProductThumbnail = (props) => {
             <div class="w-full relative rounded-xl">
                 <figure>
                     {props.product.images.filter((image, index) => index < 1).map((image, index) =>
-                        <img src={image.file ? process.env.REACT_APP_FILE_PATH + image.file.path : image.image_url} key={index} className="rounded-xl w-full h-28 object-cover" />
+                        <img src={image.file ? process.env.REACT_APP_FILE_PATH + image.file.path : image.image_url} key={index} className="rounded-xl w-full h-auto md:h-40 object-cover" />
                     )}
                 </figure>
 
@@ -17,15 +17,11 @@ export const ShortProductThumbnail = (props) => {
                 <div class="absolute top-0 left-0 h-full w-full" onClick={e => { props.moveForward("/product/" + props.product.id) }}>
                     <div className="h-full w-full flex items-center justify-center flex-col hover:bg-base-200/60">
                         <div className="grow"></div>
-                        <div class="tooltip" data-tip={props.product.name}>
-                            <div className="text-md pb-2 truncate">
+                        <div class="tooltip w-full tooltip-bottom" data-tip={props.product.name}>
+                            <div className="text-md p-2 whitespace-normal bg-slate-900/80 text-white cursor-pointer rounded-b-xl w-full text-center">
                                 {props.product.name}
                             </div>
                         </div>
-                        
-                        {/* <RateDisplayByNumber rating={props.product.ratings_avg_rate ? parseFloat(props.product.ratings_avg_rate) : 0} /> */}
-
-                        {/* <div className="btn btn-primary btn-sm mt-4" onClick={e => { props.moveForward("/product/" + props.product.id) }}>View</div> */}
                     </div>
                 </div>
 
